501b2c1af3b469d58fc612b63fe94e6cf9f8c367
gaoxinzhao@gmail.com <gaoxinzhao@gmail.com@2525d228-2a63-2530-70e3-9624e5c51a39>
2012-07-08 11:21:25 +0000

    update code

--- trunk/sites/all/modules/_custom/crf_case/js/crf_common.js
+++ trunk/sites/all/modules/_custom/crf_case/js/crf_common.js
@@ -1,3 +1,14 @@
+if (!Array.indexOf) {
+  Array.prototype.indexOf = function(obj) {
+    for (var i=0; i<this.length; i++) {
+      if (this[i]==obj) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
+
 $(document).ready(function() {
   var p = Drupal.settings.progress;
   $('#sidebar-left #block-nice_menus-1 li a').each(function(){
