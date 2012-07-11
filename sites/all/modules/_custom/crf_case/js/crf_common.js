c110fcd3f903a3d48c0f3a9ea0c9af52864e864f
gaoxinzhao <gaoxinzhao@gmail.com>
2012-07-11 03:10:25 +0000

    fixed bug

--- sites/all/modules/_custom/crf_case/js/crf_common.js
+++ sites/all/modules/_custom/crf_case/js/crf_common.js
@@ -11,7 +11,7 @@ if (!Array.indexOf) {
 
 $(document).ready(function() {
   var p = Drupal.settings.progress;
-  $('#sidebar-left #block-nice_menus-1 li a').each(function(){
+  $('#sidebar-left .nice-menus li a').each(function(){
     var href = $(this).attr('href');
     var key  = href.split('/').pop();
     if (p.indexOf(key) != -1) {
