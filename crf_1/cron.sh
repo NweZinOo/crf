6d7d52913c13a117df502a7a5dca987199f0ef28
gaoxinzhao <gaoxinzhao@gmail.com>
2012-09-13 03:22:20 +0000

    deleted old db

--- crf_1/cron.sh
+++ crf_1/cron.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+date
+DRUSH_PHP=/www/wdlinux/php/bin/php
+export DRUSH_PHP
+export PATH=$PATH:/home/robbin/bin/
+
 drush cron
 svn add sites/default/files/backup_migrate/scheduled/*
 svn ci  sites/default/files/backup_migrate/scheduled/* -m 'add backup files'
+date
+
+drush cms --mode=formal
+
 
